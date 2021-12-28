840194906e30c9b5876db4f37de889c46edcb99c
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2021-12-29 00:20:26 +0300

    refactor: local state is not used

--- src/components/AuthCallback.vue
+++ src/components/AuthCallback.vue
@@ -6,12 +6,8 @@ export default {
   name: 'AuthCallback',
   async mounted() {
     try {
-      const localState = await this.$auth.loginCallback(window.location.href)
-      if (localState && localState.location) {
-        window.location.replace(localState.location)
-      } else {
-        this.$router.replace('/')
-      }
+      await this.$auth.loginCallback(window.location.href)
+      this.$router.replace('/')
     } catch (e) {
       console.error(e)
     }
